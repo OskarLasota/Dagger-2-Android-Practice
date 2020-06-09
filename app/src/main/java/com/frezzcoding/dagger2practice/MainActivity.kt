fae8f4c477439bc3d44f0b7a350e3cfdecfead4b
Oskar Lasota <55768328+OskarLasota@users.noreply.github.com>
2020-06-09 19:38:58 +0100

    chore: injected data dependency into mainactivity

--- app/src/main/java/com/frezzcoding/dagger2practice/MainActivity.kt
+++ app/src/main/java/com/frezzcoding/dagger2practice/MainActivity.kt
@@ -2,11 +2,21 @@ package com.frezzcoding.dagger2practice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import javax.inject.Inject
+import javax.inject.Named
 
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    @field:Named("Love") lateinit var loveInfo : Info
+
+    @Inject
+    @field:Named("Hate") lateinit var hateInfo : Info
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        DaggerBoxComponent.builder().build().inject(this)
+
     }
 }
